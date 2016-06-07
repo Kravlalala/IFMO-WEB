39fea490592601654e2d7ecbeefba88cd31fa80e
Ilya Kravchuk <kravlala.1@gmail.com>
2016-06-07 20:40:46 +0800

    without last task

--- src/CSVParsing.java
+++ src/CSVParsing.java
@@ -4,22 +4,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by 1 on 07.06.2016.
  */
 public class CSVParsing extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
         String area = request.getParameter("task");
-        try (FileReader freader = new FileReader("Sports.csv")) {
+        try (Reader freader = new InputStreamReader(new FileInputStream("Sports.csv"), "UTF-8")) {
             CSVReader csv_reader = new CSVReader(freader);
             String[] nextLine;
             int i = 0;
             while ((nextLine = csv_reader.readNext()) != null) {
                 if(area.equals(nextLine[2])){
-                    response.getWriter().println(nextLine[3]+nextLine[4]);
+                    response.getWriter().println(nextLine[3]);
                 }
                 i++;
             }
